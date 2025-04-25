7435aecba43c4837d5dcd350d4384f2792162116
F-kite <ilichevv_v845@mail.ru>
2025-04-25 21:50:24 +0700

    some changes in admin panel

--- app/frontend/src/components/AdminPanel/AdminPanel.tsx
+++ app/frontend/src/components/AdminPanel/AdminPanel.tsx
@@ -15,12 +15,34 @@ import {
   SelectValue,
   SelectGroup,
 } from "@/components/ui/select";
+
+import {
+  Calculator,
+  Calendar,
+  CreditCard,
+  Settings,
+  Smile,
+  User,
+} from "lucide-react";
+
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command";
+
 import { Button } from "@/components/ui/button";
 
 import administrationApi from "@/lib/api/administration";
 import { MyContext } from "@/lib/hooks/MyContextProvider";
 import styles from "./styles.module.scss";
 import { Combobox } from "../ui/custom/combobox";
+import groupApi from "@/lib/api/groups";
 
 type UsersInfoProps = {
   user_id: number;
@@ -29,6 +51,11 @@ type UsersInfoProps = {
   created_at: string;
   role: string;
   group: string | null;
+};
+
+type GroupListProps = {
+  value: string;
+  label: string;
 };
 
 const columnsValue = {
@@ -67,9 +94,11 @@ export default function AdminPanel() {
   if (!context) {
     throw new Error("MyContext must be used within a MyProvider");
   }
-  //   const { userInfo, currentInfo } = context;
+  const { userInfo } = context;
   const [usersInfo, setUsersInfo] = useState<UsersInfoProps[]>([]);
   const [editableRowId, setEditableRowId] = useState<number | null>(null);
+  const [groupsList, setGroupsList] = useState<GroupListProps[]>();
+  const [val, setVal] = useState<number>(0);
 
   useEffect(() => {
     const fetchAllUsersInfo = async () => {
@@ -100,11 +129,36 @@ export default function AdminPanel() {
     fetchAllUsersInfo();
   }, [setUsersInfo]);
 
-  function DropDownList(currentEl: { currentEl: string }) {
+  useEffect(() => {
+    const fetchAllGroups = async () => {
+      try {
+        const response = await groupApi.getAllGroups();
+        // console.log(response);
+        const data: GroupListProps[] = [];
+        if (Array.isArray(response)) {
+          response.map((el) => {
+            data.push({
+              value: el.name,
+              label: el.name,
+            });
+          });
+          setGroupsList(data);
+        } else {
+          console.error("Response is not an array:", response);
+        }
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      }
+    };
+
+    fetchAllGroups();
+  }, [setGroupsList]);
+
+  function DropDownList({ currentEl }: { currentEl: string }) {
     return (
       <Select>
         <SelectTrigger className="w-[160px] bg-auth text-auth-foreground shadow hover:bg-auth/90">
-          <SelectValue placeholder={`${currentEl.currentEl}`} />
+          <SelectValue placeholder={`${currentEl}`} />
         </SelectTrigger>
         <SelectContent>
           <SelectGroup>
@@ -117,80 +171,159 @@ export default function AdminPanel() {
     );
   }
 
+  const setNumber = (target: number) => {
+    const value: number = target < 0 ? 1 : target > 20 ? 20 : target;
+    setVal(value);
+  };
+
+  function ScheduleAdditionPanel() {
+    return (
+      <div className={styles.additionPanel}>
+        Расписание для группы{" "}
+        <Combobox
+          elements={groupsList ? groupsList : []}
+          currentEl={userInfo.group}
+        />{" "}
+        на
+        <span>
+          <input
+            type="number"
+            onChange={(event) => setNumber(Number(event.target.value))}
+            value={val}
+            min={1}
+            max={20}
+            className={styles.input}
+          />{" "}
+          - ю
+        </span>
+        неделю
+        <Button variant={"auth"} className={styles.button}>
+          Добавить
+        </Button>
+      </div>
+    );
+  }
+
+  function CommandDemo() {
+    return (
+      <Command className={styles.command}>
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Suggestions">
+            <CommandItem>
+              <Calendar />
+              <span>Calendar</span>
+            </CommandItem>
+            <CommandItem>
+              <Smile />
+              <span>Search Emoji</span>
+            </CommandItem>
+            <CommandItem disabled>
+              <Calculator />
+              <span>Calculator</span>
+            </CommandItem>
+          </CommandGroup>
+          <CommandSeparator />
+          <CommandGroup heading="Settings">
+            <CommandItem>
+              <User />
+              <span>Profile</span>
+              <CommandShortcut>⌘P</CommandShortcut>
+            </CommandItem>
+            <CommandItem>
+              <CreditCard />
+              <span>Billing</span>
+              <CommandShortcut>⌘B</CommandShortcut>
+            </CommandItem>
+            <CommandItem>
+              <Settings />
+              <span>Settings</span>
+              <CommandShortcut>⌘S</CommandShortcut>
+            </CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </Command>
+    );
+  }
+
   if (usersInfo.length > 0)
     return (
       <div className={styles.container}>
-        <Table className={styles.table}>
-          <TableHeader>
-            <TableRow className={styles.tableRow}>
-              {Object.entries(usersInfo[0]).map(([key]) => {
-                const columnKey = key as keyof typeof columnsValue;
-                return (
-                  <TableHead className={styles.headerCell}>
-                    {columnsValue[columnKey]}
-                  </TableHead>
-                );
-              })}
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {usersInfo.map((info) => (
-              <TableRow key={info.user_id} className={styles.tableRow}>
-                {Object.entries(info).map(([key, value]) => {
-                  type RoleKeys = keyof typeof valueRolesOnRus;
+        <ScheduleAdditionPanel />
+        <div className={styles.tableContainer}>
+          <Table className={styles.table}>
+            <TableHeader>
+              <TableRow className={styles.tableRow}>
+                {Object.entries(usersInfo[0]).map(([key]) => {
+                  const columnKey = key as keyof typeof columnsValue;
                   return (
-                    <TableCell key={key} className={styles.tableCell}>
-                      {editableRowId === info.user_id ? (
-                        key == "group" ? (
-                          <Combobox
-                            elements={elements}
-                            currentEl={info[key]}
-                          ></Combobox>
-                        ) : key == "role" ? (
-                          <DropDownList
-                            currentEl={valueRolesOnRus[info[key] as RoleKeys]}
-                          />
-                        ) : (
-                          value
-                        )
-                      ) : value != null ? (
-                        value in valueRolesOnRus ? (
-                          valueRolesOnRus[value as RoleKeys]
-                        ) : (
-                          value
-                        )
-                      ) : (
-                        "-"
-                      )}
-                    </TableCell>
+                    <TableHead className={styles.headerCell}>
+                      {columnsValue[columnKey]}
+                    </TableHead>
                   );
                 })}
-                <TableCell className={styles.cellButtons}>
-                  <Button
-                    variant={"auth"}
-                    className={styles.button}
-                    onClick={() => {
-                      if (editableRowId === info.user_id)
-                        setEditableRowId(null);
-                      else setEditableRowId(info.user_id);
-                    }}
-                  >
-                    update
-                  </Button>
-                  {editableRowId === info.user_id ? (
-                    <Button variant={"auth"} className={styles.button}>
-                      save
-                    </Button>
-                  ) : (
-                    <Button variant={"auth"} className={styles.button}>
-                      delete
-                    </Button>
-                  )}
-                </TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+            </TableHeader>
+            <TableBody>
+              {usersInfo.map((info) => (
+                <TableRow key={info.user_id} className={styles.tableRow}>
+                  {Object.entries(info).map(([key, value]) => {
+                    type RoleKeys = keyof typeof valueRolesOnRus;
+                    return (
+                      <TableCell key={key} className={styles.tableCell}>
+                        {editableRowId === info.user_id ? (
+                          key == "group" ? (
+                            <Combobox
+                              elements={groupsList ? groupsList : []}
+                              currentEl={info[key]}
+                            />
+                          ) : key == "role" ? (
+                            <DropDownList
+                              currentEl={valueRolesOnRus[info[key] as RoleKeys]}
+                            />
+                          ) : (
+                            value
+                          )
+                        ) : value != null ? (
+                          value in valueRolesOnRus ? (
+                            valueRolesOnRus[value as RoleKeys]
+                          ) : (
+                            value
+                          )
+                        ) : (
+                          "-"
+                        )}
+                      </TableCell>
+                    );
+                  })}
+                  <TableCell className={styles.cellButtons}>
+                    <Button
+                      variant={"auth"}
+                      className={styles.button}
+                      onClick={() => {
+                        if (editableRowId === info.user_id)
+                          setEditableRowId(null);
+                        else setEditableRowId(info.user_id);
+                      }}
+                    >
+                      Изменить
+                    </Button>
+                    {editableRowId === info.user_id ? (
+                      <Button variant={"auth"} className={styles.button}>
+                        Сохранить
+                      </Button>
+                    ) : (
+                      <Button variant={"auth"} className={styles.button}>
+                        Удалить
+                      </Button>
+                    )}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
       </div>
     );
 }
